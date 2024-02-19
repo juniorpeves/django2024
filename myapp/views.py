a1c10a699f332492c5fa05c88dd01f6ad551fb69
juniorpeves <juniorpeves@gmail.com>
2024-02-18 19:45:54 -0500

    Vinculación-de-templates-con-views

--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,31 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>Index page</h2>")
+    title = "Django Course!"
+    return render(request,'index.html', {
+        'title': title,
+    })
 
 def hello(request, username):
     return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
-    return HttpResponse("<h2>About</h2>")
+    username = '@juniorpeves'
+    return render(request,'about.html',{
+        'username': username,
+    })
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request,'projects.html', {
+        'projects': projects,
+    })
 
-def tasks(request, id):
-    # task = Task.objects.get(id=id)
-    task = get_object_or_404(Task, id=id)
-    return HttpResponse("tasks: %s" % task.title)
+def tasks(request):
+    # task = get_object_or_404(Task, id=id)
+    return render(request,'tasks.html') 
  
